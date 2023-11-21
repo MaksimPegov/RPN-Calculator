f73dc0f0e9e924954466d0294410a304983fef38
Maksim Pegov <maksim.peg@gmail.com>
2023-11-21 10:10:15 +0100

    Project publication

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+	bool isExpanding = false;
+	double num1 = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnButtosHandler(object sender, EventArgs e)
+	{
+		Button button = (Button)sender;
+		string buttonValue = button.Text;
+		switch(buttonValue){
+			case "CE":
+				ResultEntry.Text = "";
+				break;
+			case "C":
+				ResultEntry.Text = ResultEntry.Text[..^1];
+				break;
+			case "=":
+				RunOperation();
+				break;
+			default:
+				ResultEntry.Text += buttonValue;
+				break;
+		}
+
+	}
+
+	private void RunOperation()
+	{
+		if(isExpanding)
+		{
+			Exp();
+			return;
+		}
+		BasicValidation(ResultEntry.Text);
+
+		ResultEntry.Text = RPNCalculator.Calculate(ResultEntry.Text).ToString();
+	}
+
+	private void Submit(object sender, EventArgs e)
+	{
+		RunOperation();
+	}
+
+	private void Exp(object sender, EventArgs e)
+	{
+		Exp();
+	}
+
+	private void Exp()
+	{
+		string input = ResultEntry.Text;
+
+		BasicValidation(input);
+
+		if(isExpanding)
+		{
+			ResultEntry.Text = Math.Pow(num1, Converter(input)).ToString("F2");
+			isExpanding = false;
+		}else{
+			num1 = Converter(input);
+			ResultEntry.Text = "";
+			isExpanding = true;
+		}
+	}
+	
+
+	private double Converter(string input)
+	{
+		if(!double.TryParse(input, out double num1)){
+			DisplayAlert("Error", "Can not convert input to number ", "OK");
+			return 0;
+		}
+		return num1;
+	}
+
+	private bool BasicValidation(string input)
+	{
+		string pattern = @"^[0-9()+*/.-]+$";
+		bool isEntryValid = Regex.IsMatch(input, pattern);
+		if (!isEntryValid)
+		{
+			DisplayAlert("Error", "Input has invalid symbols ", "OK");
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ Calculator/RPNCalculator.cs
@@ -0,0 +1,125 @@
+﻿namespace Calculator;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class RPNCalculator
+{
+    public static double Calculate(string input)
+    {
+        var tokens = Tokenize(input);
+        var rpn = ConvertToRPN(tokens);
+        return EvaluateRPN(rpn);
+    }
+
+  private static List<string> Tokenize(string input)
+  {
+      return Regex.Matches(input, @"[\+\-\*/\(\)]|\d+")
+                  .Cast<Match>()
+                  .Select(match => match.Value)
+                  .ToList();
+  }
+
+
+    private static List<string> ConvertToRPN(List<string> infixTokens)
+    {
+        var output = new List<string>();
+        var operatorStack = new Stack<string>();
+
+        foreach (var token in infixTokens)
+        {
+            if (double.TryParse(token, out _))
+            {
+                output.Add(token);
+            }
+            else if (IsOperator(token))
+            {
+                while (operatorStack.Count > 0 && Precedence(operatorStack.Peek()) >= Precedence(token))
+                {
+                    output.Add(operatorStack.Pop());
+                }
+                operatorStack.Push(token);
+            }
+            else if (token == "(")
+            {
+                operatorStack.Push(token);
+            }
+            else if (token == ")")
+            {
+                while (operatorStack.Count > 0 && operatorStack.Peek() != "(")
+                {
+                    output.Add(operatorStack.Pop());
+                }
+                operatorStack.Pop();
+            }
+        }
+
+        while (operatorStack.Count > 0)
+        {
+            output.Add(operatorStack.Pop());
+        }
+
+        return output;
+    }
+
+    private static double EvaluateRPN(List<string> rpnTokens)
+    {
+        var operandStack = new Stack<double>();
+
+        foreach (var token in rpnTokens)
+        {
+            if (double.TryParse(token, out var operand))
+            {
+                operandStack.Push(operand);
+            }
+            else if (IsOperator(token))
+            {
+                var operand2 = operandStack.Pop();
+                var operand1 = operandStack.Pop();
+                operandStack.Push(ApplyOperator(token, operand1, operand2));
+            }
+        }
+
+        return operandStack.Pop();
+    }
+
+    private static bool IsOperator(string token)
+    {
+        return token == "+" || token == "-" || token == "*" || token == "/";
+    }
+
+    private static int Precedence(string op)
+    {
+        switch (op)
+        {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return 0;
+        }
+    }
+
+    private static double ApplyOperator(string op, double operand1, double operand2)
+    {
+        switch (op)
+        {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                throw new ArgumentException("Invalid operator: " + op);
+        }
+    }
+}
+
